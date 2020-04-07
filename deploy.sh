993916c5012975a8b002a6fa796f7c43ed367d83
lahiyam <londonchaim@gmail.com>
2020-04-07 15:51:46 -0700

    testing deploy script modifications

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 npm install --silent --no-progress
+ls
+ls node_modules
 cd $CODEBUILD_SRC_DIR/services/$service || exit
 serverless deploy --stage $env --package \
 $CODEBUILD_SRC_DIR/services/$service/target/$env -v -r $AWS_REGION
