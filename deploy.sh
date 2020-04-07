9a85beaa43db43e320ef147b35116110c199d402
lahiyam <londonchaim@gmail.com>
2020-04-07 15:45:13 -0700

    added package.json to build artifacts dir

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-npm install -g serverless serverless-bundle serverless-offline
+npm install --silent --no-progress
 cd $CODEBUILD_SRC_DIR/services/$service || exit
 serverless deploy --stage $env --package \
 $CODEBUILD_SRC_DIR/services/$service/target/$env -v -r $AWS_REGION
