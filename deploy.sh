cfb9cca34c85337adbee5223de65a207dc7d2f9a
lahiyam <londonchaim@gmail.com>
2020-04-07 15:28:45 -0700

    updated deploy script to cd into service directory

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 npm install -g serverless
+cd $CODEBUILD_SRC_DIR/services/$service || exit
 serverless deploy --stage $env --package \
-$CODEBUILD_SRC_DIR/target/$env -v -r us-east-1
+$CODEBUILD_SRC_DIR/services/$service/target/$env -v -r $AWS_REGION
