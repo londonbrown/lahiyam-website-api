e15aac95f5a8f38535a943020f2162baca40436b
lahiyam <londonchaim@gmail.com>
2020-04-07 15:51:46 -0700

    testing deploy script modifications

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
-npm install --silent --no-progress
-ls
-ls node_modules
+npm install -g serverless
+npm install --save-dev serverless-bundle serverless-offline
 cd $CODEBUILD_SRC_DIR/services/$service || exit
 serverless deploy --stage $env --package \
 $CODEBUILD_SRC_DIR/services/$service/target/$env -v -r $AWS_REGION
