4f5aa16bb2d2b6d1bda698a7d38232e9ec024b20
lahiyam <londonchaim@gmail.com>
2020-04-07 20:49:52 -0700

    added serverless and plugins to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-npm install --save
+npm install -g serverless
+npm install --save-dev serverless-bundle serverless-offline serverless-mocha-plugin
 cd $CODEBUILD_SRC_DIR/services/$service || exit
-npm install --save
 serverless deploy --stage $env --package \
 $CODEBUILD_SRC_DIR/services/$service/target/$env -v -r $AWS_REGION
