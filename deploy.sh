5628e3a5b02150a6f8cf4357665b636fed3ac41d
lahiyam <londonchaim@gmail.com>
2020-04-07 20:28:36 -0700

    updated deploy and buildspec to use package.json files

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 npm install -g serverless
-npm install --save-dev serverless-bundle serverless-offline
+npm install
 cd $CODEBUILD_SRC_DIR/services/$service || exit
+npm install
 serverless deploy --stage $env --package \
 $CODEBUILD_SRC_DIR/services/$service/target/$env -v -r $AWS_REGION
